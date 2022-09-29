25bce36b66810d6f25372813277951e28bb68534
LucioFurnari <luciofurnari99@gmail.com>
2022-09-29 16:18:07 -0300

    Add function to calculate the operations

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const displayCalculator = document.querySelector(".result-display");
 const numberBtns = document.querySelectorAll(".number-container button")
 const operatorBtns = document.querySelectorAll(".operators-container button");
 
-const firstArrayNumbers = [];
-const secondArrayNumbers = [];
+let firstArrayNumbers = [];
+let secondArrayNumbers = [];
 let isOperatorCheck = false;
 let firstNumber;
 let secondNumber;
+let isResultValid = false;
+let selectOperator;
 
 function add(numOne,numTwo){
     return numOne + numTwo;
@@ -31,12 +33,13 @@ function getNumber(value){
         firstArrayNumbers.push(parseInt(value));
         numbers = firstArrayNumbers.join("");
         displayCalculator.textContent = numbers;
-        return numbers;
+        
+        return parseInt(numbers);
     } else {
         secondArrayNumbers.push(parseInt(value));
         numbers = secondArrayNumbers.join("");
         displayCalculator.textContent = numbers;
-        return numbers
+        return parseInt(numbers);
     }
 };
 
@@ -45,27 +48,72 @@ function getNumber(value){
 numberBtns.forEach(button => {
     button.addEventListener("click",(event) => {
         if(!isOperatorCheck){
-            firstNumber = getNumber(event.target.value)
+            firstNumber = getNumber(event.target.value);
         } else {
             secondNumber = getNumber(event.target.value);
         }
-        console.log(firstNumber);
-        console.log(secondNumber);
     });
 });
+
 
 operatorBtns.forEach(button => {
     button.addEventListener("click",(event) => {
         isOperatorCheck = true;
         switch (event.target.value) {
+            case "clean":
+                firstArrayNumbers = [];
+                secondArrayNumbers = [];
+                firstNumber ="";
+                secondNumber ="";
+                displayCalculator.textContent = "";
             case "+":
-                console.log(operate(add,firstNumber,secondNumber));
+                selectOperator = "+";
+                isResultValid = true;
                 break;
             case "-":
-                console.log(operate(subtract,firstNumber,secondNumber));
+                selectOperator = "-";
+                isResultValid = true;
+                break;
+            case "*":
+                selectOperator = "*";
+                isResultValid = true;
+                break;
+            case "/":
+                selectOperator = "/"
+                isResultValid = true;
                 break;
             default:
                 break;
         }
+
+        if(isResultValid && event.target.value == "result"){
+            isOperatorCheck = false;
+            switch (selectOperator) {
+                case "+":
+                    displayCalculator.textContent = operate(add,firstNumber,secondNumber);
+                    firstArrayNumbers = [];
+                    secondArrayNumbers = [];
+
+                    break;
+                case "-":
+                    displayCalculator.textContent = operate(subtract,firstNumber,secondNumber);
+                    firstArrayNumbers = [];
+                    secondArrayNumbers = [];
+                    break;
+                case "*":
+                    displayCalculator.textContent = operate(multiply,firstNumber,secondNumber);
+                    firstArrayNumbers = [];
+                    secondArrayNumbers = [];
+                    break;
+                case "/":
+                    displayCalculator.textContent = operate(divide,firstNumber,secondNumber);
+                    firstArrayNumbers = [];
+                    secondArrayNumbers = [];
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        })
     })
-});
